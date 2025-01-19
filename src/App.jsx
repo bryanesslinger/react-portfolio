acb39ff1ef4e71dffbe66d36325abb8a172fa249
bryanesslinger <bryanessl@gmail.com>
2025-01-18 21:03:15 -0500

    updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Project from "./components/Project";
-import './app.css';
+import './App.css';
 
 const App = () => {
   // State for form fields
