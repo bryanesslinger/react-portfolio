c5a55677c574aea49d26b3bb3bcbf3ac25023443
bryanesslinger <bryanessl@gmail.com>
2025-01-24 20:33:18 -0500

    updated font and made profile image clickable to return to home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,13 @@ const Header = () => {
   return (
     <header className="header">
       <div className="profile-container">
+        <a href="/">
         <img
           className="profile-img"
           src="/assets/cartoon.png"
           alt="Bryan Esslinger"
         />
+        </a>
         <h1>Bryan Esslinger</h1>
       </div>
       <Navigation />
