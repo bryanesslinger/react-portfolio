a7005d24df2d13e2063927028eae319e4797a5d9
bryanesslinger <bryanessl@gmail.com>
2025-06-05 19:29:56 -0400

    more updates to resume, adding pdf viewer

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 // Set up the worker for react-pdf
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js`;
 
 const Resume = () => {
   const [numPages, setNumPages] = useState(null);
